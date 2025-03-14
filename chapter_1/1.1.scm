497f475aaeea7a144052950014230b3b939e7cd5
Nick De la Cruz <48021240+nickdelacruz@users.noreply.github.com>
2025-03-14 15:08:09 +0800

    Edited and Added:
    I added one exercise and revised previous ones to improve formatting.

--- chapter_1/1.1.scm
+++ chapter_1/1.1.scm
@@ -1,21 +1,50 @@
-10 ;10
-(+ 5 3 4) ;12
-(- 9 1) ;8
-(/ 6 2) ;3
-(+ (* 2 4) (- 4 6)) ;10
-(define a 3) ;3
-(define b (+ a 1)) ;4
-(+ a b (* a b)) ;19
-(= a b) = false
+; Exercise 1.1: Below is a sequence of expressions. What is the result printed
+; by the interpreter in response to each expression? Assume that the sequence
+; is to be evaluated in the order in which it is presented.
+
+10 
+;10
+
+(+ 5 3 4) 
+;12
+
+(- 9 1) 
+;8
+
+(/ 6 2) 
+;3
+
+(+ (* 2 4) (- 4 6)) 
+;10
+
+(define a 3) 
+;3
+
+(define b (+ a 1)) 
+;4
+
+(+ a b (* a b)) 
+;19
+
+(= a b)
+;false
+
 (if (and (> b a) (< b (* a b)))
   b
-  a) ;returns b
+  a) 
+;b
+
 (cond ((= a 4) 6)
       ((= b 4) (+ 6 7 a))
-      (else 25)) ; returns 16
-(+ 2 (if (> b a) b a)) ; 6
+      (else 25)) 
+;16
+
+(+ 2 (if (> b a) b a)) 
+;6
+
 (* (cond ((> a b) a)
          ((< a b) b)
          (else -1))
-   (+ a 1)) ; 16
+   (+ a 1)) 
+;16
 
--- chapter_1/1.4.scm
+++ chapter_1/1.4.scm
@@ -1,3 +1,12 @@
+; Exercise 1.4: Observe that our model of evaluation allows for combinations whose operators
+; are compound expressions. Use this observation to describe the behaviour of
+; the following procedure:
+
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
+
+; this procedure uses a compound expression that returns a/an '-' or '+' operator
+; to the body once the if-statement has been evaluated. if b is greater-than 
+; zero, then the '+' operator will be used to evaluate variables 'a' and 'b'. If
+; b is less-than zero, the '-' operator is returned.
 
